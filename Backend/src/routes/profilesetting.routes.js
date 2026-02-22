be45feb07588faeb2c4323fec364c150f4e87509
ZenOrbits <zenorbits29@gmail.com>
2026-02-22 10:25:35 +0530

    New router post edit profile

--- Backend/src/routes/profilesetting.routes.js
+++ Backend/src/routes/profilesetting.routes.js
@@ -4,6 +4,6 @@ const profileSettingController = require('../controller/profilesetting.controlle
 const middleware = require('../middleware/middleware');
 
 router.delete('/deleteprofile',middleware.authMiddleware,profileSettingController.deleteUser);
-router.patch('/editprofile',middleware.authMiddleware,profileSettingController.editUserProfile);
+router.post('/editprofile',middleware.authMiddleware,profileSettingController.editUserProfile);
 
 module.exports = router;
