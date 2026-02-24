4eae24afba58e5277ef11b7c2a882948b4b17663
ZenOrbits <zenorbits29@gmail.com>
2026-02-24 09:05:03 +0530

    restpassword api in redux

--- Frontend/src/redux/api/resetpasswordApi.js
+++ Frontend/src/redux/api/resetpasswordApi.js
@@ -4,15 +4,22 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const resetPasswordApi = createApi({
     reducerPath: 'forgotPasswordApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://study-orbit-backend.onrender.com/api/password', credentials: 'include' }),
-    endpoints:(build)=>({
-        forgotPassword:build.mutation({
-            query:(email)=>({
-                url:'/forgotpassword',
-                method:'POST',
-                body:email
+    endpoints: (build) => ({
+        forgotPassword: build.mutation({
+            query: (email) => ({
+                url: '/forgotpassword',
+                method: 'POST',
+                body: email
+            })
+        }),
+        resetPassword: build.mutation({
+            query: ({token, password}) => ({
+                url: '/resetpassword',
+                method: 'POST',
+                body: { token, password }
             })
         })
     })
 });
 
-export const {useForgotPasswordMutation} = resetPasswordApi;
+export const { useForgotPasswordMutation, useResetPasswordMutation } = resetPasswordApi;
